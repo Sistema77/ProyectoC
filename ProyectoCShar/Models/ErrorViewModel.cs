4d6b18b1c3ede53032f32259c1c9deae8b17eb92
Jose Manuel <josemanuelmorenoguerra96@gmail.com>
2024-02-12 21:05:00 +0100

    first commit

---
+++ ProyectoCShar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoCShar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
